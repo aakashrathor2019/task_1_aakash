d6b12582343079e22886afaa408a632211039af6
Aakash Rathor <aakashrathor2019@gmail.com>
2024-12-10 15:02:20 +0530

    Add home_page and dashboard

--- Task_Management_System/urls.py
+++ Task_Management_System/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns=[
   path('login/',Login.as_view(),name='login'),
-  path('signup/',Signup.as_view(),name='signup'),
+  path('',Signup.as_view(),name='signup'),
   path('create_task/',Create_Task.as_view(),name='create_task'),
   path('home_page/',Home_Page.as_view(),name='home_page'),
   path('logout/',Logout.as_view(),name='logout'),
